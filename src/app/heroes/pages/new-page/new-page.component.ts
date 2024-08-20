dfdf93e485d0696188876c371a53d45312712d48
ana-marz <amluquin@hotmail.es>
2024-08-20 13:35:39 +0200

    agregar heroe html

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { Hero, Publisher } from '../../interfaces/hero';
 
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class NewPageComponent {
+  public publishers = [
+    { id: 'DC Comics', value: 'DC - Comics' },
+    { id: 'Marvel Comics', value: 'Marvel - Comics' },
+  ];
 
+  public hero!: Hero;
 }
